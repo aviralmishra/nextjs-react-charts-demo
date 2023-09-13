ffc3defd3c57f00c3357e155655ccdfdec33d634
Aviral Mishra <aviral.mishra@gmail.com>
2023-09-13 17:43:55 +1000

    weather data component

--- app/weather/page.js
+++ app/weather/page.js
@@ -1,3 +1,19 @@
-export default function Page() {
-    return <h1>Weather</h1>
+import Link from 'next/link'
+import Image from 'next/image'
+
+import { getWeatherData } from '/lib/weather-data'
+
+const Page = async () => {
+    const response = await getWeatherData()
+
+    return (
+        <>
+            <h1>Weather</h1>
+            <section>
+                <h2>{response.city} ({response.country})</h2>
+            </section>
+        </>
+    )
 }
+
+export default Page
